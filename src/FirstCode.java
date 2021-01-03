203f377864301055196568e8060b12ee40607851
masud <csemasud808@gmail.com>
2021-01-03 23:08:16 +0600

    working on for loop

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -1,6 +1,14 @@
 
 public class FirstCode {
 	public static void main(String args[]) {
-		System.out.print("Hello World");
+		
+		for (int i=1; i<=5; i++)
+		{
+			for (int j=1; j<=5; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
 }
