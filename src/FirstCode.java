b8e1996093d7595422215baeb1b38dc144633164
masudhossaincse <csemasud808@gmail.com>
2021-01-04 11:09:45 +0600

    working on nested loop

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -1,14 +1,14 @@
 
 public class FirstCode {
+
 	public static void main(String args[]) {
-		
-		for (int i=1; i<=5; i++)
-		{
-			for (int j=1; j<=5; j++)
-			{
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
+
 		}
 	}
 }
