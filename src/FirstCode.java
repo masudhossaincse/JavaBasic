e7a777467afcb9a6502caec2206404955c2a888b
masudhossaincse <csemasud808@gmail.com>
2021-01-04 11:21:06 +0600

    working on nested loop examples

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -5,7 +5,7 @@ public class FirstCode {
 
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= 5; j++) {
-				System.out.print("* ");
+				System.out.print(j + " ");
 			}
 			System.out.println();
 
